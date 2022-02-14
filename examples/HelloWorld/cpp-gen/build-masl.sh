74e6fb4a7b9d65bdaa0b1b6e3ae50822668b410b
Levi Starrett <levi@roxsoftware.com>
2022-02-14 14:55:57 -0500

    Update project setup

--- examples/HelloWorld/cpp-gen/build-masl.sh
+++ examples/HelloWorld/cpp-gen/build-masl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MASLBASE="docker run -it -v $PWD:/workspace levistarrett/masl-base"
 MASLMC="docker run -v $PWD:/workspace levistarrett/masl-compiler"
-$MASLMC -mod /workspace/masl/HelloWorld/HelloWorld.mod -output /workspace/cpp-gen/src/HelloWorld
-$MASLMC -domainpath /workspace/masl/HelloWorld -prj /workspace/masl/HelloWorld_proc/HelloWorld_proc.prj -output /workspace/cpp-gen/src/HelloWorld_proc
-$MASLBASE bash -c "cd cpp-gen && cmake . -DCMAKE_INSTALL_PREFIX=/workspace/cpp-gen"
+$MASLMC -mod masl/HelloWorld_OOA/HelloWorld.mod -output cpp-gen/src/HelloWorld && \
+$MASLMC -domainpath masl/HelloWorld_OOA -prj masl/HelloWorld_proc/HelloWorld_proc.prj -output cpp-gen/src/HelloWorld_proc && \
+$MASLBASE bash -c "cd cpp-gen && cmake . -DCMAKE_INSTALL_PREFIX=/workspace/cpp-gen" && \
 $MASLBASE bash -c "cd cpp-gen && make install"
