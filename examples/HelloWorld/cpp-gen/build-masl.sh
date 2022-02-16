81f2e23b7a08afe4e637c609a2aa50c307fc9df3
Levi Starrett <levi@roxsoftware.com>
2022-02-16 13:23:18 -0500

    Change build scripts to include utility domains

--- examples/HelloWorld/cpp-gen/build-masl.sh
+++ examples/HelloWorld/cpp-gen/build-masl.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-MASLBASE="docker run -it -v $PWD:/workspace levistarrett/masl-base"
-MASLMC="docker run -v $PWD:/workspace levistarrett/masl-compiler"
-$MASLMC -mod masl/HelloWorld_OOA/HelloWorld.mod -output cpp-gen/src/HelloWorld && \
-$MASLMC -domainpath masl/HelloWorld_OOA -prj masl/HelloWorld_proc/HelloWorld_proc.prj -output cpp-gen/src/HelloWorld_proc && \
-$MASLBASE bash -c "cd cpp-gen && cmake . -DCMAKE_INSTALL_PREFIX=/workspace/cpp-gen" && \
-$MASLBASE bash -c "cd cpp-gen && make install"
+MASLMC="docker run -v $PWD:/workspace levistarrett/masl-compiler -skiptranslator Sqlite"
+$MASLMC -domainpath /root/masl/utils -mod masl/HelloWorld_OOA/HelloWorld.mod -output cpp-gen/src/HelloWorld && \
+$MASLMC -domainpath /root/masl/utils:masl/HelloWorld_OOA -prj masl/HelloWorld_proc/HelloWorld_proc.prj -output cpp-gen/src/HelloWorld_proc && \
+docker run -it -v $PWD/cpp-gen:/workspace levistarrett/masl-build
