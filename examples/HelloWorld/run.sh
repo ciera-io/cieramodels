c7674ad78537cc5e6ee0c7b485db6d77d9b176cd
Levi Starrett <levi@roxsoftware.com>
2022-03-25 11:47:07 -0400

    updated esamples and integrity test to use v3.0.0-beta1

--- examples/HelloWorld/run.sh
+++ examples/HelloWorld/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CIERA_VERSION=3.0.0-SNAPSHOT
+CIERA_VERSION=3.0.0-beta1
 VERSION=1.0.0-SNAPSHOT
 MVN_REPO=$HOME/.m2/repository
 MODULEPATH=$MVN_REPO/io/ciera/runtime-api/$CIERA_VERSION/runtime-api-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/HelloWorld_OOA/$VERSION/HelloWorld_OOA-$VERSION.jar:$MVN_REPO/io/ciera/HelloWorld_proc/$VERSION/HelloWorld_proc-$VERSION.jar
--- examples/calculator/run.sh
+++ examples/calculator/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CIERA_VERSION=3.0.0-SNAPSHOT
+CIERA_VERSION=3.0.0-beta1
 VERSION=1.0.0-SNAPSHOT
 MVN_REPO=$HOME/.m2/repository
 MODULEPATH=$MVN_REPO/io/ciera/runtime-api/$CIERA_VERSION/runtime-api-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/ALU/$VERSION/ALU-$VERSION.jar:$MVN_REPO/io/ciera/calculator_proc/$VERSION/calculator_proc-$VERSION.jar
