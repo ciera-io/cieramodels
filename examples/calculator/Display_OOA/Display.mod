6564ffb0ba368b8d83bd130ccba852b9be9bf196
Levi Starrett <levi@roxsoftware.com>
2022-06-29 11:43:10 -0400

    Add Display domain for calculator for true multi-domain testing

---
+++ examples/calculator/Display_OOA/Display.mod
@@ -0,0 +1,6 @@
+domain Display is
+
+  public service result ( value: in real );
+  public service error ( message: in string );
+
+end domain;
--- examples/calculator/run.sh
+++ examples/calculator/run.sh
@@ -2,7 +2,7 @@
 CIERA_VERSION=3.0.0-SNAPSHOT
 VERSION=1.0.0-SNAPSHOT
 MVN_REPO=$HOME/.m2/repository
-MODULEPATH=$MVN_REPO/io/ciera/runtime-api/$CIERA_VERSION/runtime-api-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/ALU/$VERSION/ALU-$VERSION.jar:$MVN_REPO/io/ciera/calculator_proc/$VERSION/calculator_proc-$VERSION.jar
+MODULEPATH=$MVN_REPO/io/ciera/runtime-api/$CIERA_VERSION/runtime-api-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$MVN_REPO/io/ciera/ALU/$VERSION/ALU-$VERSION.jar:$MVN_REPO/io/ciera/Display/$VERSION/Display-$VERSION.jar:$MVN_REPO/io/ciera/calculator_proc/$VERSION/calculator_proc-$VERSION.jar
 PROPS="\
   -Dio.ciera.runtime.logLevel=FINEST \
   -Dio.ciera.runtime.useDeterministicIDs\
