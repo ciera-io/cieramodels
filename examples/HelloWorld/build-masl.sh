0c889c756a228042e9c03d6cf112b845774fbb4a
Levi Starrett <levi@roxsoftware.com>
2023-01-31 10:02:17 -0500

    update hello world to work with latest masl build

--- examples/HelloWorld/build-masl.sh
+++ examples/HelloWorld/build-masl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-MASLMC="docker run -v $PWD:/workspace levistarrett/masl-compiler -skiptranslator Sqlite"
-$MASLMC -domainpath /root/masl/utils:. -mod STRING_OOA/STRING.mod -output _build/src/STRING && \
-$MASLMC -domainpath /root/masl/utils:. -mod HelloWorld_OOA/HelloWorld.mod -output _build/src/HelloWorld && \
-$MASLMC -domainpath /root/masl/utils:. -prj HelloWorld_proc/HelloWorld_proc.prj -output _build/src/HelloWorld_proc && \
-cp CMakeLists.txt _build
-docker run -it -v $PWD/_build:/workspace levistarrett/masl-build
+MASLMC="docker run -v $PWD:/root levistarrett/masl-compiler -skiptranslator Sqlite"
+$MASLMC -domainpath /opt/masl/lib/masl:. -mod HelloWorld_OOA/src/main/masl/HelloWorld_OOA/HelloWorld.mod -output _build/src/HelloWorld && \
+$MASLMC -domainpath /opt/masl/lib/masl:HelloWorld_OOA/src/main/masl -prj HelloWorld_proc/src/main/masl/HelloWorld_proc.prj -output _build/src/HelloWorld_proc && \
+cp CMakeLists.txt _build && \
+docker run -it -v $PWD/_build:/root levistarrett/masl-build
