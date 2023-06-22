cc79e9326649f9f66a060a007577778ad665a614
Levi Starrett <levi@roxsoftware.com>
2023-06-22 12:29:35 -0400

    split build script

--- examples/HelloWorld/build-masl.sh
+++ examples/HelloWorld/build-masl.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-MASLMC="docker run -v $PWD:/root levistarrett/masl-compiler -skiptranslator Sqlite"
-$MASLMC -domainpath /opt/masl/lib/masl:. -mod HelloWorld_OOA/src/main/masl/HelloWorld_OOA/HelloWorld.mod -output _build/src/HelloWorld && \
-$MASLMC -domainpath /opt/masl/lib/masl:HelloWorld_OOA/src/main/masl -prj HelloWorld_proc/src/main/masl/HelloWorld_proc.prj -output _build/src/HelloWorld_proc && \
-cp CMakeLists.txt _build && \
 docker run -it -v $PWD/_build:/root levistarrett/masl-build
