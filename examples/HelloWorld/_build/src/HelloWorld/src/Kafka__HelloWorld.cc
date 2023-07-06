73dba5670b93c6f70caac6467fcd9c99bdbeae37
Levi Starrett <levi@roxsoftware.com>
2023-07-06 16:57:07 -0400

    TMP2

---
+++ examples/HelloWorld/_build/src/HelloWorld/src/Kafka__HelloWorld.cc
@@ -0,0 +1,33 @@
+//
+// File: Kafka__HelloWorld.cc
+//
+
+#include "HelloWorld_OOA/__HelloWorld_services.hh"
+#include "kafka/Kafka.hh"
+
+namespace Kafka {
+namespace masld_HelloWorld {
+
+bool registerServices() {
+  const int x = 7;
+  Kafka::KafkaHandler::getInstance().addSubscription(
+      "HelloWorld_multiply_by_two", [=]() {
+        ::masld_HelloWorld::interceptor_masls_multiply_by_two::instance()
+            .callService()(x);
+      });
+  Kafka::KafkaHandler::getInstance().addSubscription(
+      "HelloWorld_add_five", [=]() {
+        ::masld_HelloWorld::interceptor_masls_add_five::instance()
+            .callService()(x);
+      });
+
+  return true;
+}
+
+} // namespace masld_HelloWorld
+} // namespace Kafka
+
+namespace {
+bool masld_HelloWorld_registered =
+    ::Kafka::masld_HelloWorld::registerServices();
+}
